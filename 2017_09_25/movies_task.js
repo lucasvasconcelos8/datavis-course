25c2cb78162905762a3a00f194420a4cde99c947
Lucas Vasconcelos <lucas.vasconcelos@nex2me.com.br>
2017-10-02 13:41:07 -0300

    Add comments and fixings bugs

--- 2017_09_25/movies_task.js
+++ 2017_09_25/movies_task.js
@@ -1,3 +1,4 @@
+//function to create bars graphs with a json data, the attribute to measure and a comparator function
 function graphBars(id, data, attribute, comparator) {
 
 	d3.select("#"+id).selectAll("div.h-bar")
@@ -7,6 +8,7 @@ function graphBars(id, data, attribute, comparator) {
 		.attr("class", "h-bar")
 		.append("span")
 
+	//transform type div to number for color pattern
 	let numberTo = {
 		"box_office" : 1,
 		"budget" : 2,
@@ -14,12 +16,13 @@ function graphBars(id, data, attribute, comparator) {
 		"box_office_by_genre" : 4
 	}
 
-  let pixels = window.screen.width
+  	let pixels = window.screen.width
 
 	let max = d3.max(data, function(d) {
 		return d[attribute]
 	})
 
+	//create a range of colors for each graph
 	let colorScale = d3.scaleLinear()
         .domain([1, 4])
         .range(["#ff0234", "#04a4c7"]);
@@ -42,12 +45,14 @@ function graphBars(id, data, attribute, comparator) {
             .sort(comparator(attribute));
 }
 
+//function to compare attribute in descending order
 var compare = function(attribute) {
 	return function(a, b) {
 		return a[attribute] < b[attribute] ? 1 : -1
 	}
 }
 
+//function to calculate the profit from each movie (Box Office - Budget)
 var moviesProfit = function(data) {
 	for (var i=0; i < data.length; i++) {
 		data[i].Profit = data[i].Worldwide_Gross_M - data[i].Budget_M
@@ -55,14 +60,19 @@ var moviesProfit = function(data) {
 	return data
 }
 
+//function to group movies by genre and the box office for each genre
 var groupByGenre = function(data) {
+
 	var genres = {}
+	//iterate in each data and sum box_office for each genre
 	for (var i=0; i < data.length; i++) {
 		if(genres[data[i].Genre] == undefined)
 			genres[data[i].Genre] = 0
 		genres[data[i].Genre] += data[i].Worldwide_Gross_M
 	}
 
+	//create a list of dicts with the genre and your respective box_office(SUM). 
+	//OBS: the field with genre name is "Film" because it helps to generate graph bars with one function
 	var dataGenres = []
 	Object.keys(genres).forEach(function(key, index) {
 		dataGenres.push({
